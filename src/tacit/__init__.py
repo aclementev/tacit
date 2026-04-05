bda70b8a262390ca84602cee748aad9edf56d081
Alvaro Clemente <clemente.varo@gmail.com>
2026-04-05 19:16:03 +0200

    fix: include version as module variable

--- src/tacit/__init__.py
+++ src/tacit/__init__.py
@@ -1,5 +1,8 @@
+from importlib.metadata import version
+
 from .constraints import Check, Nullable
 from .contract import contract
 from .schema import DataFrame, Schema
 
+__version__ = version("tacit")
 __all__ = ["Check", "DataFrame", "Nullable", "Schema", "contract"]
